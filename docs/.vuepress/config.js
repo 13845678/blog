801cdeac6ca73696a0a769f82243f822ca3cec59
13845678 <xnkyn@qq.com>
2021-03-16 11:16:05 +0800

    博客插件使用

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    theme: 'reco',
+    themeConfig: {
+        type: 'blog',
+        friendLink: [
+            {
+                title: 'vuepress-theme-reco',
+                desc: 'A simple and beautiful vuepress Blog & Doc theme.',
+                logo: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+                link: 'https://vuepress-theme-reco.recoluan.com'
+            },
+            {
+                title: '午后南杂',
+                desc: 'Enjoy when you can, and endure when you must.',
+                email: 'recoluan@qq.com',
+                link: 'https://www.recoluan.com'
+            },
+            // ...
+        ],
+        themeConfig: {
+            authorAvatar: '/avatar.png'
+        },
+        // 博客配置
+        blogConfig: {
+            category: {
+                location: 2,     // 在导航栏菜单中所占的位置，默认2
+                text: 'Category' // 默认文案 “分类”
+            },
+            tag: {
+                location: 3,     // 在导航栏菜单中所占的位置，默认3
+                text: 'Tag'      // 默认文案 “标签”
+            },
+            socialLinks: [     // 信息栏展示社交信息
+                { icon: 'reco-github', link: 'https://github.com/recoluan' },
+                { icon: 'reco-npm', link: 'https://www.npmjs.com/~reco_luan' }
+            ]
+        }
+    }
+}
